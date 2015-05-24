2ae621f62fdb758682ea08de33096f452efccfb7
bhqasx <815810395@qq.com>
2015-05-24 21:29:14 +0800

    Create app.js
    1st of creating a function map. finished reading the "Capturing Mouse Movement" part of http://www.webreference.com/programming/javascript/mk/column2/index.html

---
+++ app.js
@@ -0,0 +1,14 @@
+document.onmousemove = mouseMove; 
+	function mouseMove(ev){ 
+	    ev           = ev || window.event; 
+	    var mousePos = mouseCoords(ev); 
+	} 
+	function mouseCoords(ev){ 
+	    if(ev.pageX || ev.pageY){ 
+         return {x:ev.pageX, y:ev.pageY}; 
+	    } 
+	    return { 
+	        x:ev.clientX + document.body.scrollLeft - document.body.clientLeft, 
+	        y:ev.clientY + document.body.scrollTop  - document.body.clientTop 
+	    }; 
+	} 
